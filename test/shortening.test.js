649c6b944bd56a43cf4b63955f5aa596f91db70b
Karīna Dzene <karina.dzene@gmail.com>
2023-06-05 13:19:11 +0300

    added folders

--- test/shortening.test.js
+++ test/shortening.test.js
@@ -1,27 +1,28 @@
-const shortening = require("./shortening");
+const shortening = require("../src/shortening");
 
 test("should return converted value, if a valid desiredValueUnit is given.", () => {
-    expect(
-        shortening(123, "K")
-    ).toStrictEqual({value: 0.123, valueUnit: "K"});
+    expect(shortening(123, "K"))
+        .toStrictEqual({value: 0.123, valueUnit: "K"});
 })
 
 test("should return value and valueUnit as-is, if an invalid desiredValueUnit is passed as a parameter.", () => {
-    expect(
-        shortening(1234, "not a real unit")
-    ).toStrictEqual({value: 1234, valueUnit: "not a real unit"});
+    expect(shortening(1234, "not a real unit"))
+        .toStrictEqual({value: 1234, valueUnit: "not a real unit"});
 })
 
 test("should return converted value and largest valueUnit which the value has, if the desiredValueUnit is undefined or null.", () => {
-    expect(shortening(999999999)).toStrictEqual({value: 999.999999, valueUnit: "M"})
+    expect(shortening(999999999))
+        .toStrictEqual({value: 999.999999, valueUnit: "M"});
 })
 
 test("should return value and valueUnit as-is, if the given value itself is invalid.", () => {
-    expect(shortening(undefined, "K")).toStrictEqual({value: undefined, valueUnit: "K"})
+    expect(shortening(undefined, "K"))
+        .toStrictEqual({value: undefined, valueUnit: "K"});
 })
 
 test("should return value as-is and valueUnit as \"\", if the given value itself is invalid and the desiredValueUnit is undefined or null.", () => {
-    expect(shortening(undefined)).toStrictEqual({value: undefined, valueUnit: ""})
+    expect(shortening(undefined))
+        .toStrictEqual({value: undefined, valueUnit: ""});
 })
 
 
